eb4bffa854ffc3da55a1624242a8ef696de5d09f
U-keun Song <sukeun319@gmail.com>
2023-05-25 19:06:18 +0900

    router package를 component로 스캔하도록 지정

--- src/main/java/com/ukeun/springbootwebflux/SpringBootWebfluxApplication.java
+++ src/main/java/com/ukeun/springbootwebflux/SpringBootWebfluxApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.ukeun.springbootwebflux.controller")
+@ComponentScan({"com.ukeun.springbootwebflux.handler","com.ukeun.springbootwebflux.router"})
 public class SpringBootWebfluxApplication {
 
     public static void main(String[] args) {
